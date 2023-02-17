ffd872974953009f66850bf3d2ac7bd23e67cc9a
Lauramarie Laskey <ll00098@my.westga.edu>
2023-02-17 15:29:11 -0500

    edits to Tilebag class

--- tile_bag.rb
+++ tile_bag.rb
@@ -37,9 +37,4 @@ class TileBag
     # hash map for tiles and their respective points
     letters = {:A => 1, :B => 3, :C => 3, :D => 2, :E => 1, :F => 4, :G => 2, :H => 4, :I => 1, :J => 8, :K => 5, :L => 1, :M => 3, :N => 1, :O => 1, :P => 3, :Q => 10, :R => 1, :S => 1, :T => 1, :U => 1, :V => 4, :W => 4, :X => 8, :Y => 4, :Z => 10}
 
-	# a method for objects of this class
-	# note that to_s is Ruby's version of toString()
-	def to_s
-		"#{@date.to_s} Title: #{@title} Description: #{@desc}"
-	end
 end
