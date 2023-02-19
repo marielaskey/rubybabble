f0bd808f4aa4435951eedb13d0eb1f0ca3423eff
Lauramarie Laskey <ll00098@my.westga.edu>
2023-02-19 09:36:33 -0500

    Edits to tile_bag + changes to unit tests

--- tests/tile_bag/test_points_for.rb
+++ tests/tile_bag/test_points_for.rb
@@ -1,17 +1,17 @@
-require 'tile_bag'
+require_relative "../../tile_bag.rb"
+require 'minitest/autorun'
 
 # unit tests for the TileBag::points_for method. 
-class TileBag::TestPointsFor
+class TileBag::TestPointsFor < MiniTest::Test
  
     # checks all 26 tile values to confirm they return the proper points
     def test_confirm_point_values
-        letters = [:A => 1, :B => 3, :C => 3, :D => 2, :E => 1, :F => 4, :G => 2, :H => 4, 
+        tiles = {:A => 1, :B => 3, :C => 3, :D => 2, :E => 1, :F => 4, :G => 2, :H => 4, 
             :I => 1, :J => 8, :K => 5, :L => 1, :M => 3, :N => 1, :O => 1, :P => 3, :Q => 10, 
-            :R => 1, :S => 1, :T => 1, :U => 1, :V => 4, :W => 4, :X => 8, :Y => 4, :Z => 10]
-        for a in :A..:Z
-        points[a] = TileBag.points_for(a)
+            :R => 1, :S => 1, :T => 1, :U => 1, :V => 4, :W => 4, :X => 8, :Y => 4, :Z => 10}
+        tiles.each do |letter, points|
+            assert_equal( points, TileBag.points_for(letter) )
         end
-        assert_equal(letters.values, points)
     end
 
 end
--- tile_bag.rb
+++ tile_bag.rb
@@ -1,42 +1,67 @@
 class TileBag
 	# (optional) generator for getters/setters for our instance vars
-	attr_accessor :title, :desc, :date
+	attr_accessor :tiles
 
 	# the initialize method is the constructor
-	def initialize bag, tiles
-        @bag = bag
-        @tiles = Array.new(:A, :A, :A, :A, :A, :A, :A, :A, :A, :B, :B, :C, :C,
-            :D, :D, :D, :D, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E, 
-            :F, :F, :G, :G, :G, :H, :H, :I, :I, :I, :I, :I, :I, :I, :I, :I, :J, 
-            :K, :L, :L, :L, :L, :M, :M, :N, :N, :N, :N, :N, :N, :O, :O, :O, :O,
-            :O, :O, :O, :O, :P, :P, :Q, :R, :R, :R, :R, :R, :R, :S, :S, :S, :S, 
-            :T, :T, :T, :T, :T, :T, :U, :U, :U, :U, :V, :V, :W, :W, :X, :Y, :Y, :Z)
-	
-    end
+	def initialize
+        @tiles = Array.new()
+
+            12.times {|n| @tiles.push(:E)}
+            9.times {|n| @tiles.push(:A)}
+            9.times {|n| @tiles.push(:I)}
+            8.times {|n| @tiles.push(:O)}
+            6.times {|n| @tiles.push(:N)}
+            6.times {|n| @tiles.push(:R)}
+            6.times {|n| @tiles.push(:T)}
+            4.times {|n| @tiles.push(:L)}
+            4.times {|n| @tiles.push(:S)}
+            4.times {|n| @tiles.push(:U)}
+            4.times {|n| @tiles.push(:D)}
+            3.times {|n| @tiles.push(:G)}
+            2.times {|n| @tiles.push(:B)}
+            2.times {|n| @tiles.push(:C)}
+            2.times {|n| @tiles.push(:M)}
+            2.times {|n| @tiles.push(:P)}
+            2.times {|n| @tiles.push(:F)}
+            2.times {|n| @tiles.push(:H)}
+            2.times {|n| @tiles.push(:V)}
+            2.times {|n| @tiles.push(:W)}
+            2.times {|n| @tiles.push(:Y)}
+            1.times {|n| @tiles.push(:K)}
+            1.times {|n| @tiles.push(:J)}
+            1.times {|n| @tiles.push(:X)}           
+            1.times {|n| @tiles.push(:Q)}           
+            1.times {|n| @tiles.push(:Z)}           
+        
+        end
 
     # removes one random tile from the bag and returns it
     def draw_tile
-        return @tiles.delete_at(rand(0..@tiles.length-1))
+        remaining = @tiles.length
+        idx = rand(0..(remaining - 1))
+        tile = @tiles.delete_at(idx)
+#puts "Drawing from #{remaining} tiles at: #{idx}, value: #{tile}"
+        return tile
+        #return @tiles.delete_at(rand(0..(@tiles.length - 1)))
     end
 
     #returns true if the bag is empty, false otherwise
     def empty?
-        if bag == 0
+        if @tiles.length == 0
             return true
             else
                 return false
+        end
     end
 
     # this is a class method. The tile parameter is a symbol 
     # representing a letter (:A, :B, :C, etc.).  The method returns 
     # the points for the given tile letter.
     def self.points_for(tile)
-        return letters[tile]
+        tilePoints = {:A => 1, :B => 3, :C => 3, :D => 2, :E => 1, :F => 4, :G => 2, :H => 4, 
+            :I => 1, :J => 8, :K => 5, :L => 1, :M => 3, :N => 1, :O => 1, :P => 3, :Q => 10, 
+            :R => 1, :S => 1, :T => 1, :U => 1, :V => 4, :W => 4, :X => 8, :Y => 4, :Z => 10}
+        return tilePoints[tile]
     end
-
-    # hash map for tiles and their respective points
-    letters = {:A => 1, :B => 3, :C => 3, :D => 2, :E => 1, :F => 4, :G => 2, :H => 4, 
-        :I => 1, :J => 8, :K => 5, :L => 1, :M => 3, :N => 1, :O => 1, :P => 3, :Q => 10, 
-        :R => 1, :S => 1, :T => 1, :U => 1, :V => 4, :W => 4, :X => 8, :Y => 4, :Z => 10}
 
 end
